b22918edb2a075dfebb69c75f58de631c8491a2c
Takaaki Numai <takaaki.numai01@g.softbank.co.jp>
2023-01-16 21:42:17 +0900

    Apply format

--- src/internal_sync_test.cpp
+++ src/internal_sync_test.cpp
@@ -1,11 +1,11 @@
 #include "MOR/MovingObjectRemoval.h"
 
-int main (int argc, char** argv)
+int main(int argc, char** argv)
 {
-  ros::init (argc, argv, "test_moving_object");
+  ros::init(argc, argv, "test_moving_object");
   ros::NodeHandle nh;
 
-  MovingObjectRemoval mor(nh,"/home/wentaoq/lidar_ws/src/dynamicslamtool-master3/config/MOR_config.txt",4,3);
+  MovingObjectRemoval mor(nh, "/home/wentaoq/lidar_ws/src/dynamicslamtool-master3/config/MOR_config.txt", 4, 3);
 
   ros::spin();
 }
